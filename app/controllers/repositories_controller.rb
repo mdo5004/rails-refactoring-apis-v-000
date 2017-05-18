068df9c9adb2a395e6b4245da053b837987dfe76
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-18 10:51:28 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,11 @@
 class RepositoriesController < ApplicationController
     def index
-        ghs = GithubService.new({access_token:session[:token]})
+        ghs = GithubService.new("access_token" => session[:token])
         @repos_array = ghs.get_repos
     end
 
     def create
-        ghs = GithubService.new(access_token:session[:token])
+        ghs = GithubService.new("access_token" => session[:token])
         response = ghs.create_repo(params[:name])
         redirect_to '/'
     end
